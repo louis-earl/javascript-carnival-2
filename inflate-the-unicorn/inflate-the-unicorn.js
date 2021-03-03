072e31b92537518e1a57908f157d74075ba45684
Louis <louis.earl.14@gmail.com>
2021-03-04 12:33:00 +1300

    Completed inflate the unicorn

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,21 @@
   // -    -   -   -   -  //
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
+var unicornStages = [0, 0, 0];
 
-console.log("Inflate The Unicorn!")
+
+function inflate(index) {
+  
+  var element = document.getElementById("unicorn" + index);
+
+  if (unicornStages[index] == 3) {
+    unicornStages[index] = 0;
+    alert("Unicorn Number " + (index + 1) +" says thank you!");
+  }
+  else {
+    unicornStages[index]++;
+  }
+
+  element.src = "./images/unicorn-" + unicornStages[index] + ".png";
+
+}
